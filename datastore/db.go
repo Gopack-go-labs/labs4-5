7e92ffb5732b8df5c7de8c2d122ef6fd1e757a17
erotourtes <erotourtes@gmail.com>
2024-06-02 19:21:56 +0300

    Revert "fix: possible error on segment cleanup, when client read data with file descriptor that will be removed"
    This reverts commit 18dd7aea375778132284c49eb940e713a1d2ef15.

--- datastore/db.go
+++ datastore/db.go
@@ -252,9 +252,7 @@ func (db *Db) mergeOldSegments() error {
 	db.segments = append(shadowDb.segments, db.curSegment())
 
 	for _, segment := range segmentsToMerge {
-		segment.mu.Lock()
 		os.Remove(segment.FilePath())
-		segment.mu.Unlock()
 	}
 
 	return nil
