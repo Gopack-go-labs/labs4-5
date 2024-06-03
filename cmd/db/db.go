d8b3a9cd957e0367366aab23b740f424f182b245
Andrii Halai <halaiandriy@gmail.com>
2024-06-03 18:54:00 +0300

    Fix logic for determening the type of value in json body

--- cmd/db/db.go
+++ cmd/db/db.go
@@ -15,96 +15,93 @@ import (
 )
 
 type Res struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
-	Type  string `json:"type"`
+  Key   string `json:"key"`
+  Value string `json:"value"`
+  Type  string `json:"type"`
 }
 
 type Req struct {
-	Value string `json:"value"`
-	Type  string `json:"type"`
+  Value interface{} `json:"value"`
 }
 
 func main() {
-	httpHandler := mux.NewRouter()
+  httpHandler := mux.NewRouter()
 
-	dir, err := ioutil.TempDir("", "temp-dir")
-	if err != nil {
-		log.Fatal(err)
-	}
+  dir, err := ioutil.TempDir("", "temp-dir")
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	db, err := datastore.NewDb(dir, 10*datastore.Megabyte)
-	if err != nil {
-		log.Fatalf("Failed to initialize database: %v", err)
-	}
-	defer db.Close()
-	
-	httpHandler.HandleFunc("/db/{key}", func(rw http.ResponseWriter, req *http.Request) {
-		urlStr := req.URL.String()
-		myUrl, _ := url.Parse(urlStr)
-		params, _ := url.ParseQuery(myUrl.RawQuery)
-		
-		vars := mux.Vars(req)
-		key := vars["key"]
-	
-		switch req.Method {
-		case http.MethodGet:
-			var val string
-			var err error
-			dataType := "string"
-	
-			if params.Get("type") == "int64" {
-				var data int64
-				data, err = db.GetInt64(key)
-				val = strconv.FormatInt(data, 10)
-				dataType = "int64"
-			} else {
-				val, err = db.GetString(key)
-			}
+  db, err := datastore.NewDb(dir, 10*datastore.Megabyte)
+  if err != nil {
+    log.Fatalf("Failed to initialize database: %v", err)
+  }
+  defer db.Close()
+  
+  httpHandler.HandleFunc("/db/{key}", func(rw http.ResponseWriter, req *http.Request) {
+    urlStr := req.URL.String()
+    myUrl, _ := url.Parse(urlStr)
+    params, _ := url.ParseQuery(myUrl.RawQuery)
+    
+    vars := mux.Vars(req)
+    key := vars["key"]
+  
+    switch req.Method {
+    case http.MethodGet:
+      var val string
+      var err error
+      dataType := "string"
+  
+      if params.Get("type") == "int64" {
+        var data int64
+        data, err = db.GetInt64(key)
+        val = strconv.FormatInt(data, 10)
+        dataType = "int64"
+      } else {
+        val, err = db.GetString(key)
+      }
 
-			if err != nil {
-				rw.WriteHeader(404)
-				return
-			}
-			
-			rw.Header().Set("content-type", "application/json")
-			rw.WriteHeader(http.StatusOK)
-			_ = json.NewEncoder(rw).Encode(Res{
-				Key: key,
-				Value: val,
-				Type: dataType,
-			})
+      if err != nil {
+        rw.WriteHeader(404)
+        return
+      }
+      
+      rw.Header().Set("content-type", "application/json")
+      rw.WriteHeader(http.StatusOK)
+      _ = json.NewEncoder(rw).Encode(Res{
+        Key: key,
+        Value: val,
+        Type: dataType,
+      })
 
-		case http.MethodPost:
-			var body Req
+    case http.MethodPost:
+      var body Req
 
-			// TODO: Allow passing integers to json object
-			err := json.NewDecoder(req.Body).Decode(&body)
-			if err != nil {
-				rw.WriteHeader(http.StatusBadRequest)
-				return
-			}
+      err := json.NewDecoder(req.Body).Decode(&body)
+      if err != nil {
+        rw.WriteHeader(http.StatusBadRequest)
+        return
+      }
 
-			val, err := strconv.ParseInt(body.Value, 10, 64)
+      switch v := body.Value.(type) {
+      case string:
+        err = db.PutString(key, v)
+      case float64:
+        err = db.PutInt64(key, int64(v))
+      }
 
-			if err != nil {
-				err = db.PutString(key, body.Value)
-			} else {
-				err = db.PutInt64(key, val)
-			}
+      if err != nil {
+        rw.WriteHeader(http.StatusInternalServerError)
+        return
+      }
+      
+      rw.WriteHeader(http.StatusCreated)
+    }
+  })
 
-			if err != nil {
-				rw.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			
-			rw.WriteHeader(http.StatusCreated)
-		}
-	})
+  server := httptools.CreateServer(8083, httpHandler)
 
-	server := httptools.CreateServer(8083, httpHandler)
+  server.Start()
 
-	server.Start()
-
-	signal.WaitForTerminationSignal()
+  signal.WaitForTerminationSignal()
 }
