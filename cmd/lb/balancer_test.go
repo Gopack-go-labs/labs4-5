1e0883750c5b4200af061b42204173c50c5709e2
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2024-06-03 17:19:05 +0300

    Update tests

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,146 +1,121 @@
-package main
+package integration
 
 import (
-	"github.com/stretchr/testify/assert"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
-	"net/http/httptest"
-	"net/url"
+	"os"
+	"sync"
 	"testing"
 	"time"
 )
 
-func TestBalancer(t *testing.T) {
-	t.Run("Heart beat passes", func(t *testing.T) {
-		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-		}))
-		defer server.Close()
-		server1, _ := url.Parse(server.URL)
-
-		lb := LoadBalancerInit(
-			[]string{server1.Host},
-			100*time.Millisecond,
-			15*time.Minute,
-		)
-
-		go lb.Heartbeat()
-
-		time.Sleep(500 * time.Millisecond)
-		assert.Equal(t, true, lb.servers[0].alive)
-	})
-
-	t.Run("Heart beat fails", func(t *testing.T) {
-		timeout := 50 * time.Millisecond
-		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(timeout + 100*time.Millisecond)
-			w.WriteHeader(http.StatusOK)
-		}))
-		defer server.Close()
-		server1, _ := url.Parse(server.URL)
-
-		lb := LoadBalancerInit(
-			[]string{server1.Host},
-			100*time.Millisecond,
-			timeout,
-		)
-
-		go lb.Heartbeat()
-
-		time.Sleep(150 * time.Millisecond)
-		assert.Equal(t, false, lb.servers[0].alive)
-	})
-
-	t.Run("Forward request", func(t *testing.T) {
-		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.URL.Path == "/health" {
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-			if r.URL.Path == "/api/v1/some-data" {
-				w.WriteHeader(http.StatusOK)
-				_, e := w.Write([]byte("some data"))
-				if e != nil {
-					t.Fatal(e)
-				}
-				return
-			}
-		}))
-		defer server.Close()
-		server1, _ := url.Parse(server.URL)
-
-		lb := LoadBalancerInit(
-			[]string{server1.Host},
-			100*time.Millisecond,
-			15*time.Minute,
-		)
-
-		req := httptest.NewRequest(http.MethodGet, "/api/v1/some-data", nil)
-		w := httptest.NewRecorder()
-
-		go lb.Heartbeat()
-		time.Sleep(200 * time.Millisecond)
-
-		lb.Serve(w, req)
-		assert.Equal(t, http.StatusOK, w.Code)
-		assert.NotNil(t, w.Body)
-		assert.Equal(t, "some data", w.Body.String())
-	})
-
-	t.Run("Forward request with least connections", func(t *testing.T) {
-		server1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("server1"))
-		}))
-		defer server1.Close()
-
-		server2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("server2"))
-		}))
-		defer server2.Close()
-
-		server1URL, _ := url.Parse(server1.URL)
-		server2URL, _ := url.Parse(server2.URL)
-
-		lb := LoadBalancerInit(
-			[]string{server1URL.Host, server2URL.Host},
-			100*time.Millisecond,
-			15*time.Minute,
-		)
-
-		req1 := httptest.NewRequest(http.MethodGet, "/api/v1/some-data", nil)
-		req2 := httptest.NewRequest(http.MethodGet, "/api/v1/some-data", nil)
-		w1 := httptest.NewRecorder()
-		w2 := httptest.NewRecorder()
-
-		go lb.Heartbeat()
-		time.Sleep(200 * time.Millisecond)
-
-		go lb.Serve(w1, req1)
-		go lb.Serve(w2, req2)
-
-		time.Sleep(300 * time.Millisecond)
-		bodies := []string{w1.Body.String(), w2.Body.String()}
-		assert.Contains(t, bodies, "server1")
-		assert.Contains(t, bodies, "server2")
-	})
+type Res struct {
+  Key   string `json:"key"`
+  Value string `json:"value"`
+  Type  string `json:"type"`
 }
 
-func TestLeastConnectionFunc(t *testing.T) {
-	t.Run("EmptyServers", func(t *testing.T) {
-		server := leastConnections([]*Server{})
-		assert.Nil(t, server)
-	})
+const baseAddress = "http://balancer:8090"
+const teamName = "gopack"
 
-	t.Run("CompareServers", func(t *testing.T) {
-		servers := []*Server{{addr: "server1:8080"}, {addr: "server2:8080"}, {addr: "server3:8080"}}
-		servers[0].load.Add(3)
-		servers[1].load.Add(2)
-		servers[2].load.Add(4)
+var client = http.Client{
+  Timeout: 3 * time.Second,
+}
 
-		server := leastConnections(servers)
+func TestBalancer(t *testing.T) {
+  var wg sync.WaitGroup
+  var mu sync.Mutex
+  if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+    t.Skip("Integration test is not enabled")
+  }
 
-		assert.NotNil(t, server)
-		assert.Equal(t, servers[1], server)
-	})
+  serverHits := make(map[string]int)
+  requestCount := 100
+
+  for i := 0; i < requestCount; i++ {
+    wg.Add(1)
+    go func() {
+      defer wg.Done()
+      var body Res
+      
+      resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data?key=%s", baseAddress, teamName))
+      if err != nil {
+        t.Error("Could not get response from server")
+        return
+      }
+
+      err = json.NewDecoder(resp.Body).Decode(&body)
+      if err != nil {
+        t.Error("Cannot decode response body")
+      }
+
+      if body.Key != teamName || body.Type != "string" {
+        t.Error("Response body contains wrong information")
+      }
+
+      server := resp.Header.Get("lb-from")
+      mu.Lock()
+      serverHits[server]++
+      mu.Unlock()
+      resp.Body.Close()
+    }()
+
+  }
+  wg.Wait()
+
+  for server, hits := range serverHits {
+    t.Logf("Server %s handled %d requests", server, hits)
+
+  }
+
+  // Validate that the requests were distributed to more than one server
+  if len(serverHits) < 2 {
+    t.Error("Load was not distributed to multiple servers")
+  }
+
+  resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data?key=asdf", baseAddress))
+  if err != nil {
+    t.Error("Could not get response from server")
+    return
+  }
+
+  if resp.StatusCode != http.StatusNotFound {
+    t.Error("Wrong status code for non existing key")
+  }
+
+  if bodyBytes, _ := io.ReadAll(resp.Body); len(bodyBytes) != 0 {
+    t.Error("Body of the 404 response is not empty")
+  }
+
+  resp.Body.Close()
+}
+
+func BenchmarkBalancer(b *testing.B) {
+  var wg sync.WaitGroup
+  if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+    b.Skip("Integration benchmark is not enabled")
+  }
+
+  reqCount := 1000
+  start := time.Now()
+
+  for n := 0; n < reqCount; n++ {
+    wg.Add(1)
+    go func() {
+      defer wg.Done()
+      resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+      if err != nil {
+        b.Error(err)
+        return
+      }
+      resp.Body.Close()
+    }()
+  }
+  wg.Wait()
+
+  t := time.Now()
+  elapsed := t.Sub(start)
+  b.Logf("Processed %d requests in %v", reqCount, elapsed)
 }
